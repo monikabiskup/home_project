02b8f2448fdaa79ce7dacba67588c02b8f34cb62
monikabiskup <monika.biskup@onet.pl>
2019-01-20 20:11:39 +0100

    Dependency Injection added

--- home_project_main/src/main/java/pl/javastart/app/Main.java
+++ home_project_main/src/main/java/pl/javastart/app/Main.java
@@ -1,12 +1,21 @@
 package pl.javastart.app;
 
+import pl.javastart.dao.FileMessageProducer;
 import pl.javastart.dao.MessagePrinter;
+import pl.javastart.dao.MessageProducer;
+import pl.javastart.dao.SimpleMessageProducer;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        MessagePrinter printer = new MessagePrinter();
-        printer.printMessage();
+        MessageProducer fileMsgProducer = new FileMessageProducer();
+        MessagePrinter filePrinter = new MessagePrinter(fileMsgProducer);
+        filePrinter.printMessage();
+
+        MessageProducer simpleMsgProducer = new SimpleMessageProducer();
+        MessagePrinter simplePrinter = new MessagePrinter(simpleMsgProducer);
+        simplePrinter.printMessage();
+
     }
 }
--- home_project_main/src/main/java/pl/javastart/dao/MessagePrinter.java
+++ home_project_main/src/main/java/pl/javastart/dao/MessagePrinter.java
@@ -2,12 +2,15 @@ package pl.javastart.dao;
 
 public class MessagePrinter {
 
-    private MessageProducer messageProducer = new SimpleMessageProducer();
+    private MessageProducer messageProducer;
+
+    public MessagePrinter (MessageProducer messageProducer) {
+        this.messageProducer = messageProducer;
+    }
 
     public void printMessage() {
         String message = messageProducer.getMessage();
         System.out.println(message);
     }
 }
-
 
